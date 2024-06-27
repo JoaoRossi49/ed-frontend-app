bb9a79b027f0ac4cb702f11b58058cba29600b1a
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2024-06-26 22:01:14 -0300

    refactor: limpeza de arquivos desnecessários

--- src/examples/Sidenav/index.js
+++ src/examples/Sidenav/index.js
@@ -39,6 +39,8 @@ import SidenavCollapse from "examples/Sidenav/SidenavCollapse";
 import SidenavRoot from "examples/Sidenav/SidenavRoot";
 import sidenavLogoLabel from "examples/Sidenav/styles/sidenav";
 
+import CloseIcon from '@mui/icons-material/Close';
+
 // Material Dashboard 2 React context
 import {
   useMaterialUIController,
@@ -157,19 +159,11 @@ function Sidenav({ color, brand, brandName, routes, ...rest }) {
           sx={{ cursor: "pointer" }}
         >
           <MDTypography variant="h6" color="secondary">
-            <Icon sx={{ fontWeight: "bold" }}>close</Icon>
+            <CloseIcon/>
           </MDTypography>
         </MDBox>
         <MDBox component={NavLink} to="/" display="flex" alignItems="center">
-          {brand && <MDBox component="img" src={brand} alt="Brand" width="2rem" />}
-          <MDBox
-            width={!brandName && "100%"}
-            sx={(theme) => sidenavLogoLabel(theme, { miniSidenav })}
-          >
-            <MDTypography component="h6" variant="button" fontWeight="medium" color={textColor}>
-              {brandName}
-            </MDTypography>
-          </MDBox>
+          {brand && <MDBox component="img" src={brand} alt="Brand" width="8rem" />}
         </MDBox>
       </MDBox>
       <Divider
